89a8cd7d7fadd2c64962ceafa76806ff5a248637
ThomasMoly <tpierre2007@icloud.com>
2025-04-17 14:08:17 -0400

    completed books, working on TvShows

--- src/pages/TVShows.jsx
+++ src/pages/TVShows.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../CSS/TVShows.css'
+import Search from '../components/search'
 
 const TVShows = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+
   return (
-    <div className='text-sky-50'>TVShows</div>
+    <main>
+      <div className="patternG"/>
+      <div className="wrapper">
+        <header>
+          <img src='../TV-img.png' alt='hero'/>
+
+            <h1>Find <span className="text-gradient2">TVshows</span> You'll Enjoy Without The Hassle</h1>
+            <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm} />  
+
+        </header>
+      </div>
+    </main>
   )
 }
 
